11631b654bdbe3b161127784d49a6ef450a4ec85
juice <ju1cecfr@gmail.com>
2021-06-21 08:39:38 -0700

    address some layout styling and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,40 @@ module.exports = {
   theme: {
     backgroundColor: theme => ({
       ...theme('colors'),
-      'primary': '#3490dc',
+      'primary': '#FFBF00',
+      'secondary': '#ffed4a',
+      'danger': '#e3342f',
+    }),
+    textColor: theme => ({
+      ...theme('colors'),
+      'primary': '#FFBF00',
       'secondary': '#ffed4a',
       'danger': '#e3342f',
     }),
     container: {
-      center: true,
+      maxWidth: {
+        DEFAULT: '1240px',
+      },
+      padding: {
+        DEFAULT: '1.25rem',
+        sm: '1rem',
+      },
     },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   }
 }
