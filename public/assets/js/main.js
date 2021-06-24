258248a3c0421fcb82dcdac175239ac63f04a81c
dire <diredeaths@gmail.com>
2021-06-24 10:24:30 -0700

    add node-network particles

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,3 +7,5 @@
 particlesJS.load('particles-js-header', 'assets/js/particles1.json', function() { console.log('callback - particles.js config loaded'); }); 
 
 particlesJS.load('particles-js-features', 'assets/js/particles2.json', function() { console.log('callback - particles.js config loaded'); });
+
+particlesJS.load('particles-js-node-network', 'assets/js/particles3.json', function() { console.log('callback - particles.js config loaded'); });
