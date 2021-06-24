84877a113914e923b683d8655489b75207ef3716
ZiggStardust <ziggstardust@protonmail.com>
2021-06-24 22:10:12 +0100

    tracking

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,36 @@
-
-
-// particlesJS("particles-js-header", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":4,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":236.74429248968178,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"top","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":2725.8005034713888}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
-// particlesJS("particles-js-features", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":4,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":236.74429248968178,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"top","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":2725.8005034713888}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
-
+// particlesJS 
 
 particlesJS.load('particles-js-header', 'assets/js/particles1.json', function() { console.log('callback - particles.js config loaded'); }); 
 
 particlesJS.load('particles-js-features', 'assets/js/particles2.json', function() { console.log('callback - particles.js config loaded'); });
 
 particlesJS.load('particles-js-node-network', 'assets/js/particles3.json', function() { console.log('callback - particles.js config loaded'); });
+
+// Fathom Tracking 
+
+let trackedCTAPrimary = false;
+let trackedCTAHeader = false;
+    
+document.getElementById('primary-cta').onclick = function handleCTAClick(e) {
+  e.preventDefault();
+  if (!trackedCTAPrimary) {
+    trackCTAClick();
+    trackedCTAPrimary = true;
+  }
+  window.location = this.href;
+}
+
+document.getElementById('header-cta').onclick = function handleCTAClick(e) {
+  e.preventDefault();
+  if (!trackedCTAHeader) {
+    trackCTAClick();
+    trackedCTAHeader = true;
+  }
+  window.location = this.href;
+}
+
+function trackCTAClick() {
+  if (!window.fathom) return;
+  window.fathom.trackGoal('BTEYLTFG', 0);
+}
 
